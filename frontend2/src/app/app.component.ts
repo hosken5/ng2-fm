f8e65da3cd081a70575cec32cea56cebdbd4dce0
hongpengfei <hongpengfei@yimei180.com>
2017-01-20 15:49:25 +0800

    ng2-bootstrap

--- frontend2/src/app/app.component.ts
+++ frontend2/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  public totalItems: number = 64;
+  public currentPage: number = 4;
+
+  public setPage(pageNo: number): void {
+    this.currentPage = pageNo;
+  }
+
+  public pageChanged(event: any): void {
+    console.log('Page changed to: ' + event.page);
+    console.log('Number items per page: ' + event.itemsPerPage);
+  }
 }
